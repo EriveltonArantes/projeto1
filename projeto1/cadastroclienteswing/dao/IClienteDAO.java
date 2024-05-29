d8899078600fa022bb2d17060bb687603c3fedc0
Erivelton Arantes de Souza <162745958+EriveltonArantes@users.noreply.github.com>
2024-05-29 11:24:56 -0300

    Add files via upload

---
+++ projeto1/cadastroclienteswing/dao/IClienteDAO.java
@@ -0,0 +1,19 @@
+package cadastroclienteswing.dao;
+
+
+import cadastroclienteswing.domain.Cliente;
+import java.util.Collection;
+
+public interface IClienteDAO {
+    
+    public Boolean cadastrar(Cliente cliente);
+    
+    public void excluir(String cpf);
+    
+    public void alterar(Cliente cliente);
+    
+    public Cliente consultar(String cpf);
+
+    public Collection<Cliente> buscarTodos();
+    
+}
